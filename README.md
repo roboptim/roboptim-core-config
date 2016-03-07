d3fe770aacd8c0b580727a515cf32bb8cacb4a7e
Benjamin Chrétien <chretien.b@gmail.com>
2016-03-07 22:09:36 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+roboptim-core-config
+====================
+
+Helper library used to load RobOptim solver configurations from YAML files.
+
+[![License LGPL 3][badge-license]](http://www.gnu.org/licenses/lgpl-3.0.txt)
+[![Build Status](https://travis-ci.org/roboptim/roboptim-core-config.png?branch=master)](https://travis-ci.org/roboptim/roboptim-core-config)
+[![codecov.io](https://codecov.io/github/roboptim/roboptim-core-config/coverage.svg?branch=master)](https://codecov.io/github/roboptim/roboptim-core-config?branch=master)
+[![Join the chat at https://gitter.im/roboptim/development](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/roboptim/development?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+[badge-license]: https://img.shields.io/badge/license-LGPL_3-green.svg
---
+++ tests/loader.cc
@@ -0,0 +1,41 @@
+// Copyright (C) 2016 by Benjamin Chrétien, CNRS-AIST JRL.
+//
+// This file is part of roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "shared-tests/fixture.hh"
+
+#include <boost/make_shared.hpp>
+
+#include <roboptim/core/function.hh>
+
+using namespace roboptim;
+
+typedef boost::mpl::list< ::roboptim::EigenMatrixDense,
+			  ::roboptim::EigenMatrixSparse> functionTypes_t;
+
+boost::shared_ptr<boost::test_tools::output_test_stream> output;
+
+BOOST_FIXTURE_TEST_SUITE (core_config, TestSuiteConfiguration)
+
+BOOST_AUTO_TEST_CASE_TEMPLATE (loader, T, functionTypes_t)
+{
+  output = retrievePattern ("loader");
+
+  std::cout << output->str () << std::endl;
+  BOOST_CHECK (output->match_pattern ());
+}
+
+BOOST_AUTO_TEST_SUITE_END ()
